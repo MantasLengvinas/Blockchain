8d5694561bf3a6ab0c7e5b5fa20862fc745d422d
Mantas <mantaslengvin@gmail.com>
2021-10-05 18:10:06 +0300

    Created base models

---
+++ Blockchain/Blockchain/Models/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blockchain.Models
+{
+    class Transaction
+    {
+        public string TransactionID { get; }
+        public string SentFrom { get; set; }
+        public string SentTo { get; set; }
+    }
+}
